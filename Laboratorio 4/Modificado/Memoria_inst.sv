55a3b471cb682709b5337088b2f55ab7269bf3e2
Mich-007 <78453205+Mich-007@users.noreply.github.com>
2025-12-03 10:38:08 -0600

    Update Memoria_inst.sv

--- Laboratorio 4/Modificado/Memoria_inst.sv
+++ Laboratorio 4/Modificado/Memoria_inst.sv
@@ -3,7 +3,7 @@ module Memoria_inst #(
   parameter DEPTH = 512   // 512 palabras = 2 KiB (0x0000..0x07FF)
 )(
   input  logic [8:0] a,       // índice de palabra: ProgAddress_o[10:2]
-  input  logic       clk,     // opcional: si tu top no pasa clk, puedes quitarlo y hacer combinacional
+  input  logic       clk,     // ojito, se puede quitar y hacer combinacional
   output logic [31:0] spo
 );
 
@@ -11,9 +11,9 @@ module Memoria_inst #(
   logic [31:0] mem [0:DEPTH-1];
   logic [31:0] spo_reg;
 
-  // inicializar desde program.hex (colocar program.hex en la carpeta de simulación)
+  // inicializar desde program.hex
   initial begin
-    // si el archivo no existe, $readmemh no falla en todos los simuladores, pero revisa la ruta
+
     $readmemh("program.hex", mem);
   end
 
